dcc14bfd0c3d74c0cde52c0fb608754dd6765863
Dorin <gimiri99@gmail.com>
2023-03-20 22:25:54 -0400

    drawing the Checkpoint

---
+++ src/Checkpoint.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Checkpoint {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private boolean hasCar;
+
+    public Checkpoint(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.hasCar = false;
+    }
+
+    public void draw(Graphics g) {
+       g.setColor(Color.RED);
+        g.fillRect(x-5, y, width, height);
+    }
+
+    public boolean hasCar() {
+        return hasCar;
+    }
+
+    public void setHasCar(boolean hasCar) {
+        this.hasCar = hasCar;
+    }
+
+}
