254221897d277f72b829cd88dc2d15c2fce0eba3
Andrew <spezzhh@gmail.com>
2023-03-21 11:57:43 -0400

    Updated Car and Checkpoint classes

---
+++ src/Checkpoint.java
@@ -0,0 +1,17 @@
+// 3/9 - created class, added appropriate attributes/accessors/mutators [AJ]
+
+public class Checkpoint {
+    private int locationX;
+    private int locationY;
+
+    public Checkpoint(int x, int y){
+        this.locationX = x;
+        this.locationY = y;
+    }
+
+    // TODO: add destForWho
+
+    public boolean isCarHere(Car c){
+        if c
+    }
+}
