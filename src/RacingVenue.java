88c382e4dccf9f2c662e7d0700cc3f9135685c8b
Dorin <gimiri99@gmail.com>
2023-03-24 01:21:18 -0400

    diplay race results

--- src/RacingVenue.java
+++ src/RacingVenue.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 class RacingVenue extends JPanel {
 
@@ -8,39 +7,62 @@ class RacingVenue extends JPanel {
 	private Car[] cars;
 	private Checkpoint[] checkpoints;
 
-	public RacingVenue(int numCars) {
-		this.cars = createCars(numCars);
+	long startTime = System.currentTimeMillis();
+
+
+	public RacingVenue(Car[] carConfigurations) {
+		this.cars = carConfigurations;
 		this.checkpoints = new Checkpoint[cars.length];
 		for (int i = 0; i < cars.length; i++) {
 			checkpoints[i] = new Checkpoint(0, cars[i].getY(), 600, cars[i].getY());
 		}
 
-		Timer timer = new Timer(10, e -> {
+
+		Timer[] timerHolder = new Timer[1];
+		timerHolder[0] = new Timer(10, e -> {
 			for (int i = 0; i < cars.length; i++) {
-				cars[i].move(checkpoints[i]);
+				cars[i].move(checkpoints[i], startTime);
 			}
 			repaint();
+
+			if (allCarsFinished()) {
+				timerHolder[0].stop();
+				displayRaceResults();
+			}
 		});
-		timer.start();
+		timerHolder[0].start();
 
 		setPreferredSize(new Dimension(700, 400));
 		setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
 	}
 
-	private Car[] createCars(int numCars) {
-		Car[] cars = new Car[numCars];
-		Random random = new Random();
-		String[] colors = {"blue", "green", "yellow"};
-		String[] tireTypes = {"summer", "winter"};
-		Integer[] wheelSizes = {15, 17};
-
-		for (int i = 0; i < numCars; i++) {
-			String color = colors[random.nextInt(colors.length)];
-			String tireType = tireTypes[random.nextInt(tireTypes.length)];
-			int wheelSize = wheelSizes[random.nextInt(wheelSizes.length)];
-			cars[i] = new Car(color, tireType, wheelSize, 0, 50 + i * 150);
+	private boolean allCarsFinished() {
+		for (Car car : cars) {
+			if (!car.isFinished()) {
+				return false;
+			}
 		}
-		return cars;
+		return true;
+	}
+
+	private void displayRaceResults() {
+		StringBuilder results = new StringBuilder("Race results:\n");
+		int winningIndex = 0;
+		long bestTime = cars[0].getFinishTime();
+
+		for (int i = 0; i < cars.length; i++) {
+			Car car = cars[i];
+			long timeTaken = car.getFinishTime();
+			double timeTakenSeconds = timeTaken / 1000.0;
+			results.append("Car ").append(i + 1).append(": ").append(timeTakenSeconds).append(" s\n");
+			if (timeTaken < bestTime) {
+				winningIndex = i;
+				bestTime = timeTaken;
+			}
+		}
+		results.append("The winner is Car ").append(winningIndex + 1).append("!");
+
+		JOptionPane.showMessageDialog(null, results.toString(), "Race Results", JOptionPane.INFORMATION_MESSAGE);
 	}
 
 	@Override
